09a7b960d8884f698b2fa9e642c5686cd878a87f
nicole gumula <ngumula@guilford.edu>
2023-04-20 10:34:44 -0400

    First commit

---
+++ src/main/java/edu/guilford/App.java
@@ -0,0 +1,17 @@
+package edu.guilford;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+
+        
+    }
+}
